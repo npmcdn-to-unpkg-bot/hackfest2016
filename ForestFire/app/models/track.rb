6f8db2a429055a46654aa10259bd874995965d95
CDeighton <cullumdeighton@gmail.com>
2016-08-20 13:16:11 +1200

    set up rails admin

--- ForestFire/app/models/track.rb
+++ ForestFire/app/models/track.rb
@@ -1,2 +1,4 @@
 class Track < ActiveRecord::Base
+  has_many :coordinates, :dependent => :delete_all
+  accepts_nested_attributes_for :coordinates
 end
--- ForestFire/db/schema.rb
+++ ForestFire/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820005626) do
+ActiveRecord::Schema.define(version: 20160820010204) do
 
   create_table "coordinates", force: :cascade do |t|
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "track_id"
   end
 
   create_table "tracks", force: :cascade do |t|
