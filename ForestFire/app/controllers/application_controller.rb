f105c49c0bce17a9bbeb92e336e1da666e47df8a
CDeighton <cullumdeighton@gmail.com>
2016-08-20 13:53:55 +1200

    setup devise for admin auth

--- ForestFire/app/controllers/application_controller.rb
+++ ForestFire/app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :random_track
+
+  def random_track
+    offset = rand(Track.count)-1
+    # Rails 3
+    @track = Track.first(offset)
+
+    puts "TEST: " + @track.to_s
+    puts "offset: " + Track.count.to_s
+  end
 end
