9a5d382a090e21327eef67ff64e7b1649a0a0f01
CDeighton <cullumdeighton@gmail.com>
2016-08-21 11:43:33 +1200

    added user location

--- ForestFire/db/schema.rb
+++ ForestFire/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820015046) do
+ActiveRecord::Schema.define(version: 20160820231651) do
+
+  create_table "button_texts", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "coordinates", force: :cascade do |t|
     t.float    "latitude"
