5e15868de0bec5b70496eaf00764f9fb980265dc
Cameron <cambo_the_rambo@hotmail.com>
2016-08-20 14:48:33 +1200

    Printing Out Coords

--- ForestFire/app/models/track.rb
+++ ForestFire/app/models/track.rb
@@ -1,2 +1,24 @@
 class Track < ActiveRecord::Base
+
+	def load_xml
+
+		@doc = Nokogiri::XML(File.open("app/assets/WCC_Tracks.kml"))
+
+		@doc.css('Placemark').each do |placemark|
+
+  			# data = placemark.css('SimpleData')
+  			coords = placemark.at_css('coordinates')
+
+  			if coords
+
+  				coords.text.split(' ').each do |coord|
+  					(lon, lat) = coord.split(',')
+  					print "#{lat}, #{lon}"
+
+  				end
+  				puts "\n"
+  			end
+  		end
+	end
+
 end
